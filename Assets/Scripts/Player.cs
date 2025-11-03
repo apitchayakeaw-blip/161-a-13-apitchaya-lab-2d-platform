b834d0e1b0c794e118ac41002cad7cbe58b27774
apitchayakeaw-blip <apitchaya.keaw@bumail.net>
2025-11-03 20:58:08 +0700

    shoot Banana

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 
-public class Player : Character
+public class Player : Character, IShootable
 {
+    [field: SerializeField] public GameObject Bullet { get ; set ; }
+    [field: SerializeField] public Transform ShootPoint { get; set ; }
+    public float ReloadTime { get; set; }
+    public float WaitTime { get; set; }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Intialize(100);
+        ReloadTime = 1.0f;
+        WaitTime = 0.0f;
     }
 
     public void OnHitWith( Enemy enemy)
@@ -23,9 +30,27 @@ public class Player : Character
         }
     }
 
+    private void FixedUpdate()
+    {
+        WaitTime += Time.fixedDeltaTime;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        Shoot();
+    }
+
+    public void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && WaitTime >= ReloadTime )
+        {
+            var bullet = Instantiate(Bullet, ShootPoint.position, Quaternion.identity );
+            Banana banana = bullet.GetComponent<Banana>();
+            if (banana != null)
+                banana.InitWeapon(20, this);
+
+            WaitTime = 0.0f;
+        }
     }
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    public int damage;
+    public IShootable Shooter;
+    public abstract void Move();
+    public abstract void OnHitWih(Character character);
+
+    public void InitWeapon(int newDamage, IShootable newShooter)
+    {
+        damage = newDamage;
+        Shooter = newShooter;
+    }
+
+    public int GetShootDirection()
+    {
+        float value = Shooter.ShootPoint.position.x - Shooter.ShootPoint.parent.position.x;
+
+        if (value > 0)
+            return 1;
+        else return 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Character character = other.GetComponent<Character>();
+        if (character != null)
+        {
+            OnHitWih(character);
+            Destroy(this.gameObject, 5f);
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
